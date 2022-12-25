543bed644b6a00ae87a170a9a277a597fcb2185c
enesberke.inal <enesberke.inal@trendyol.com>
2022-12-25 15:34:11 +0300

    Added step 4 and styles

--- five-step-form/src/components/step/index.jsx
+++ five-step-form/src/components/step/index.jsx
@@ -23,7 +23,10 @@ function Step({
       </S.Body>
       <S.StepFooter>
       {hasBackButton &&  <S.GoBackButton onClick={onBack}>Go Back</S.GoBackButton> }
-      {hasNextButton &&   <S.GoNextButton type="submit">Next Step</S.GoNextButton> }
+
+      {hasNextButton ? ( <S.GoNextButton type="submit">Next Step</S.GoNextButton> ):
+                       (<S.GoNextButton type="submit">Confirm</S.GoNextButton>)
+   }
       </S.StepFooter>
 
     </S.Step>
